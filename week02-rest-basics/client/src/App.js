fd8587457e245b05be9d6be850d3712de0c8aa68
Elizabeth 322 <elizabeth.fuenzalida@bellevuecollege.edu>
2017-04-18 17:54:22 -0700

    updated unit test

--- week02-rest-basics/client/src/App.js
+++ week02-rest-basics/client/src/App.js
@@ -11,6 +11,14 @@ class App extends Component {
             foo: 'waiting for server',
             nine: '0',
             eight: '0',
+            seven: '0',
+            six: '0',
+            five: '0',
+            four: '0',
+            three: '0',
+            two: '0',
+            one: '0',
+
         };
 
     }
@@ -35,6 +43,29 @@ class App extends Component {
     getEight = () => {
         this.setState({eight: '8'});
     };
+
+    getSeven = () => {
+        this.setState({seven: '7'});
+    };
+    getSix = () => {
+        this.setState({six: '6'});
+    };
+    getFive = () => {
+        this.setState({five: '5'});
+    };
+    getFour = () => {
+        this.setState({four: '4'});
+    };
+    getThree = () => {
+        this.setState({three: '3'});
+    };
+    getTwo = () => {
+        this.setState({two: '2'});
+    };
+    getOne = () => {
+        this.setState({one: '1'});
+    };
+
 
     fetch() {
         this.setState({foo: "bar"});
@@ -86,11 +117,39 @@ class App extends Component {
                 <p className="App-intro">
                     state.eight: {this.state.eight}
                 </p>
+                <p className="App-intro">
+                    state.seven: {this.state.seven}
+                </p>
+                <p className="App-intro">
+                    state.six: {this.state.six}
+                </p>
+                <p className="App-intro">
+                    state.five: {this.state.five}
+                </p>
+                <p className="App-intro">
+                    state.four: {this.state.four}
+                </p>
+                <p className="App-intro">
+                    state.three: {this.state.three}
+                </p>
+                <p className="App-intro">
+                    state.two: {this.state.two}
+                </p>
+                <p className="App-intro">
+                    state.one: {this.state.one}
+                </p>
 
                 <button onClick={this.bar}>Click Me</button>
                 <button className="getFoo" onClick={this.getFoo}>Click Me</button>
                 <button className="getNine" onClick={this.getNine}>Click Me</button>
                 <button className="getEight" onClick={this.getEight}>Click Me</button>
+                <button className="getSeven" onClick={this.getSeven}>Click Me</button>
+                <button className="getSix" onClick={this.getSix}>Click Me</button>
+                <button className="getFive" onClick={this.getFive}>Click Me</button>
+                <button className="getFour" onClick={this.getFour}>Click Me</button>
+                <button className="getThree" onClick={this.getThree}>Click Me</button>
+                <button className="getTwo" onClick={this.getTwo}>Click Me</button>
+                <button className="getOne" onClick={this.getOne}>Click Me</button>
             </div>
         );
     }
--- week02-rest-basics/client/src/App.test.js
+++ week02-rest-basics/client/src/App.test.js
@@ -46,6 +46,15 @@ describe('My react basic test', function() {
         expect(wrapper.contains(nineSign)).toEqual(true);
     });
 
+    it('renders button click message for eight', () => {
+        const wrapper = shallow(<App />);
+        const eightSign = <p className="App-intro">state.eight: 8</p>;
+        wrapper.find('button.getEight').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(eightSign)).toEqual(true);
+
+    });
+
     it('renders initial value of paragraph with state.eight', () => {
         const wrapper = shallow(<App />);
         const eightSign = <p className="App-intro">state.eight: 0</p>;
@@ -54,12 +63,118 @@ describe('My react basic test', function() {
         expect(wrapper.contains(eightSign)).toEqual(true);
     });
 
-    it('renders button click message for eight', () => {
+    it('renders button click message for seven', () => {
         const wrapper = shallow(<App />);
-        const eightSign = <p className="App-intro">state.eight: 8</p>;
-        wrapper.find('button.getEight').simulate('click');
+        const sevenSign = <p className="App-intro">state.seven: 7</p>;
+        wrapper.find('button.getSeven').simulate('click');
         getLast(wrapper);
-        expect(wrapper.contains(eightSign)).toEqual(true);
+        expect(wrapper.contains(sevenSign)).toEqual(true);
 
+    });
+
+    it('renders initial value of paragraph with state.seven', () => {
+        const wrapper = shallow(<App />);
+        const sevenSign = <p className="App-intro">state.seven: 0</p>;
+        const sevenp = wrapper.find('p').last().debug();
+        console.log(sevenp);
+        expect(wrapper.contains(sevenSign)).toEqual(true);
+    });
+
+    it('renders button click message for six', () => {
+        const wrapper = shallow(<App />);
+        const sixSign = <p className="App-intro">state.six: 6</p>;
+        wrapper.find('button.getSix').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(sixSign)).toEqual(true);
+    });
+
+    it('renders initial value of paragraph with state.six', () => {
+        const wrapper = shallow(<App />);
+        const sixSign = <p className="App-intro">state.six: 0</p>;
+        const sixp = wrapper.find('p').last().debug();
+        console.log(sixp);
+        expect(wrapper.contains(sixSign)).toEqual(true);
+    });
+
+    it('renders button click message for five', () => {
+        const wrapper = shallow(<App />);
+        const fiveSign = <p className="App-intro">state.five: 5</p>;
+        wrapper.find('button.getFive').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(fiveSign)).toEqual(true);
+    });
+
+    it('renders initial value of paragraph with state.five', () => {
+        const wrapper = shallow(<App />);
+        const fiveSign = <p className="App-intro">state.five: 0</p>;
+        const fivep = wrapper.find('p').last().debug();
+        console.log(fivep);
+        expect(wrapper.contains(fiveSign)).toEqual(true);
+    });
+
+    it('renders button click message for four', () => {
+        const wrapper = shallow(<App />);
+        const fourSign = <p className="App-intro">state.four: 4</p>;
+        wrapper.find('button.getFour').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(fourSign)).toEqual(true);
+
+    });
+
+    it('renders initial value of paragraph with state.four', () => {
+        const wrapper = shallow(<App />);
+        const fourSign = <p className="App-intro">state.four: 0</p>;
+        const fourp = wrapper.find('p').last().debug();
+        console.log(fourp);
+        expect(wrapper.contains(fourSign)).toEqual(true);
+    });
+
+    it('renders button click message for three', () => {
+        const wrapper = shallow(<App />);
+        const threeSign = <p className="App-intro">state.three: 3</p>;
+        wrapper.find('button.getThree').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(threeSign)).toEqual(true);
+
+    });
+
+    it('renders initial value of paragraph with state.three', () => {
+        const wrapper = shallow(<App />);
+        const threeSign = <p className="App-intro">state.three: 0</p>;
+        const threep = wrapper.find('p').last().debug();
+        console.log(threep);
+        expect(wrapper.contains(threeSign)).toEqual(true);
+    });
+
+    it('renders button click message for two', () => {
+        const wrapper = shallow(<App />);
+        const twoSign = <p className="App-intro">state.two: 2</p>;
+        wrapper.find('button.getTwo').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(twoSign)).toEqual(true);
+    });
+
+    it('renders initial value of paragraph with state.two', () => {
+        const wrapper = shallow(<App />);
+        const twoSign = <p className="App-intro">state.two: 0</p>;
+        const twop = wrapper.find('p').last().debug();
+        console.log(twop);
+        expect(wrapper.contains(twoSign)).toEqual(true);
+    });
+
+    it('renders button click message for one', () => {
+        const wrapper = shallow(<App />);
+        const oneSign = <p className="App-intro">state.one: 1</p>;
+        wrapper.find('button.getOne').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(oneSign)).toEqual(true);
+    });
+
+    it('renders initial value of paragraph with state.one', () => {
+        const wrapper = shallow(<App />);
+        const oneSign = <p className="App-intro">state.one: 0</p>;
+        const onep = wrapper.find('p').last().debug();
+        console.log(onep);
+        expect(wrapper.contains(oneSign)).toEqual(true);
     });
 });
