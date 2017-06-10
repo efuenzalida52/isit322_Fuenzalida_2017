e3f9a96655771fd515fa846d927b085688eb2bbd
Elizabeth 322 <elizabeth.fuenzalida@bellevuecollege.edu>
2017-06-09 17:37:45 -0700

    redux basic

---
+++ reduxweek09/src/spokesman.js
@@ -0,0 +1,15 @@
+
+const  spokesman = (state = {statement: 'No comment'}, action) => {
+    switch (action.type){
+        case 'VERIFY':
+            return {statement:'We Stand by it. In fact we invented it.'};
+        case 'DENY':
+            return{statement:'We deny everything.We have never heard of it.'};
+        case 'NO COMMENT':
+            return{statement:'No comment.'};
+        default:
+            return state;
+    }
+};
+
+export  default spokesman;
